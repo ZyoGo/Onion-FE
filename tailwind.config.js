bd829473b955db0471352a20d890e718cc770738
w33h <academyynetworking@gmail.com>
2021-07-13 12:30:12 +0800

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'kaushan-script': ['Kaushan Script', 'cursive'],
+    },
+    extend: {
+      colors: {
+        darkPurple: '#6C5CE7',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
