3bfdf4abffdd45cd2b5330fbdef058b5a2c217af
w33h <academyynetworking@gmail.com>
2021-07-17 20:39:36 +0800

    add course

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    zIndex: {
+      '-10': '-10',
+    },
     fontFamily: {
       'kaushan-script': ['Kaushan Script', 'cursive'],
     },
