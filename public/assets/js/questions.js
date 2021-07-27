f7718a8bf7b0cc397fd9521cf67ddfc788d3db3a
w33h <academyynetworking@gmail.com>
2021-07-27 23:09:19 +0800

    change class

---
+++ public/assets/js/questions.js
@@ -0,0 +1,39 @@
+const questions = [
+  {
+    question: 'Apa yang di maksud dengan routing',
+    answers: [
+      {
+        text: 'mekanisme pengiriman paket data yang ditransmisikan dari satu network ke network yang lain.',
+        correct: true,
+      },
+      { text: 'Menghubungkan router ke internet', correct: false },
+      { text: 'Menghubungkan router ke internet', correct: false },
+      { text: 'Menghubungkan router ke internet', correct: false },
+    ],
+  },
+  {
+    question: 'Who is the best YouTuber?',
+    answers: [
+      { text: 'Web Dev Simplified', correct: true },
+      { text: 'Traversy Media', correct: true },
+      { text: 'Dev Ed', correct: true },
+      { text: 'Fun Fun Function', correct: true },
+    ],
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false },
+    ],
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true },
+    ],
+  },
+];
